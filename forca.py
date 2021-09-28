843811c4d3122dbeb6433721815fbd235a625582
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-09-28 07:50:14 -0300

    inicio do projeto

--- forca.py
+++ forca.py
@@ -3,6 +3,12 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
