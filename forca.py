e7df0b873de86f62f4efd541999fe2e7efb301f7
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-09-29 07:14:27 -0300

    encontrando palavras

--- forca.py
+++ forca.py
@@ -3,12 +3,18 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "python"
     enforcou = False
     acertou = False
 
+
     while(not enforcou and not acertou):
-        print("jogando...")
+        chute= input("Qual letra?")
+        index = 0
+        for letra in palavra_secreta:
+            if(chute==letra):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index = index + 1
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
